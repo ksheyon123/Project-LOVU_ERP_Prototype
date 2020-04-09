649b0549a8c35f871c8dbc4d418d22a9f717dc69
강서현 <gangseohyeon@TAG27.local>
2020-04-09 18:54:01 +0900

    todaySupply Page Ongoing

--- server/routes/logis.js
+++ server/routes/logis.js
@@ -11,10 +11,6 @@ router.get('/requestLogis', (req, res) => {
   res.render('requestLogis')
 });
 
-router.get('/logisManagement', (req, res) => {
-  res.render('logisManagement')
-});
-
 router.post('/putRequestToDB', async (req, res) => {
   try {
     console.log(req.body)
@@ -25,12 +21,25 @@ router.post('/putRequestToDB', async (req, res) => {
   }
 })
 
-router.get('/getLogisManagement', async (req, res) => {
+router.get('/todaySupply', (req, res) => {
+  res.render('todaySupply')
+})
+
+router.get('/getItemsList', async (req, res) => {
   try {
-    console.log('hi')
-    res.send('a');
+    itemList = [
+      'GNB 화이트닝 펩타 미스트',
+      'GNB 더 퍼스트 프리미엄 앰플',
+      'GNB PRO6 프리미엄 마사지 크림',
+      '닥터힐럭스 하이드라리차지 스킨',
+      '닥터힐럭스 리바이탈라이즈 세럼',
+      '닥터힐럭스 리뉴얼콤플렉스 크림',
+      '닥터힐럭스 화이트 폼클렌징',
+      '닥터힐럭스 5.8 마일드 클렌저',
+    ]
+    res.send(itemList)
   } catch (err) {
-    console.log(err)
+    console.log('err')
   }
 })
 
